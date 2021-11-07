60ef09737fddf269cc9d856be006fa7f2dd51fe0
Артем Белышев <artembelysev@MacBook-Pro-Artem.local>
2021-11-07 14:21:03 +0100

    added animation

--- scripts/script4.js
+++ scripts/script4.js
@@ -30,6 +30,30 @@ $(document).ready(function() {
 	});
 });
 
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("#order_our0").submit(function() { //Change
+    swal({
+    text: "Message was sent. Our team will contact you soon.",
+    button: "OK"
+    });
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+      console.log('done');
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+});
+
 //Popup
 const popupLinks = document.querySelectorAll(".popup_link");
 const body = document.querySelector("body");
